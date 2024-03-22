e07ec6b3307053d42c89a67a9ff3c99479a01a0e
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-03-21 19:01:34 -0600

    gave the product values

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,21 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTERGER,
+    },
+    inStock: {
+      type: DataTypes.BOOLEAN,
+    },
   },
   {
     sequelize,
