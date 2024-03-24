a39b0747b22aaacff3ea5ded250ca1322cef53ce
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-03-24 16:41:25 -0600

    added a method to destory a product in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,8 +127,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  // Deleting by a products "id" number
+  try {
+    const productData = await Product.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json('{"message": "Error deleting Product"}');
+  }
 });
 
 module.exports = router;
